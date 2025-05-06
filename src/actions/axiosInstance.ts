a8e4895b04c4514d673793b325d063be33cc2714
Svitlana-G <svitlana.golubieva@gmail.com>
2025-05-06 19:36:46 +0200

    feat: switch axsios baseURL to real API and add auth token

--- src/actions/axiosInstance.ts
+++ src/actions/axiosInstance.ts
@@ -1,9 +1,19 @@
 import axios from 'axios'
+import { useAuthStore } from 'zustand/store'
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3005/',
+  // baseURL: 'http://localhost:3005/',
+  baseURL: 'http://localhost:8080/api/',
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
+axiosInstance.interceptors.request.use((config) => {
+  const { token } = useAuthStore.getState()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 export default axiosInstance
