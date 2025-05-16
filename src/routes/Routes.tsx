1589fd9134077069030d796fad540a67dce3d7dc
Marina Kotelnikov <oce4ka@gmail.com>
2025-05-16 17:23:10 +0200

    feat: new page "There is no friends in your area", redirect

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,6 +8,7 @@ import AuthCallbackPage from 'pages/AuthCallbackPage'
 import FirstProfile from 'pages/FirstProfile'
 import Friends from 'pages/FriendsPage'
 import Invitation from '../components/invitation/Invitation'
+import ErrorMensSearch from 'pages/ErrorMensSearch'
 import ErrorPage from 'pages/ErrorPage'
 import Match from '../components/findMatch/Match'
 import Messages from 'pages/MessagesPage'
@@ -23,6 +24,8 @@ import AccountConfirmationMessage from 'pages/AccountConfirmationMessage'
 import EmailAlreadyConfirmed from 'pages/EmailAlreadyConfirmed'
 import SecurityDialogExamplePage from '../components/securityDialog/SecurityDialogExamplePage'
 import Logout from '../pages/Logout'
+import NoticeNoLikes from '../components/noticeNoData/NoticeNoLikes'
+import NoticeNoUsers from '../components/noticeNoData/NoticeNoUsers'
 
 const Loadable =
   (Component: ComponentType) => (props: JSX.IntrinsicAttributes) =>
@@ -121,6 +124,9 @@ const routes: RouteObject[] = [
     path: 'delete',
     element: <DeleteUserDialogExamplePage />,
   },
+  { path: 'notice-no-likes', element: <NoticeNoLikes /> },
+  { path: 'notice-no-users', element: <NoticeNoUsers /> },
+  { path: 'no-friends-in-your-area', element: <ErrorMensSearch /> },
   { path: 'invite', element: <Invitation /> },
   { path: 'error-400', element: <ErrorPage code={400} /> }, // Route is working for demonstration
   { path: 'error-500', element: <ErrorPage code={500} /> }, // Route is working for demonstration
