8ec00c0df2a58377c47ead274eae1ad95c57a822
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2026-04-15 14:08:11 +0200

    feat: implement Error Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useRoutes } from 'react-router-dom'
-import './App.css'
 import { ThemeProvider } from '@mui/material'
 import theme from 'styles/createTheme'
-import routes from './routes/Routes'
+import routes from 'routes/Routes'
 import { Auth0ProviderWithNavigate } from 'provider/Auth0ProviderWithNavigate'
-import GenderRedirectGuard from './components/userAuth/GenderRedirectGuard'
+import GenderRedirectGuard from 'components/userAuth/GenderRedirectGuard'
+import AppErrorBoundary from 'components/errorContent/AppErrorBoundary'
+import './App.css'
 
 const App = () => {
   const content = useRoutes(routes)
   return (
     <Auth0ProviderWithNavigate>
       <ThemeProvider theme={theme}>
-        <GenderRedirectGuard>{content}</GenderRedirectGuard>
+        <AppErrorBoundary>
+          <GenderRedirectGuard>{content}</GenderRedirectGuard>
+        </AppErrorBoundary>
       </ThemeProvider>
     </Auth0ProviderWithNavigate>
   )
