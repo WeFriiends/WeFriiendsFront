6cb7fb7b39b6a3ef8878d9c4a18737e4100cee63
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2026-09-01 23:56:08 +0200

    fix: remove blocked user from near-me and who-liked-me lists instantly

--- src/utils/updateUserLists.ts
+++ src/utils/updateUserLists.ts
@@ -1,6 +1,10 @@
 import { mutate } from 'swr'
 import { PROFILE_ENDPOINTS, LIKE_ENDPOINTS } from 'actions/endpoints'
 import { usePotentialFriendsStore, useMatchesStore } from 'zustand/friendsStore'
+import { UserMiniProfile } from 'common/types/userTypes'
+
+const removeUser = (userId: string) => (users: UserMiniProfile[] | undefined) =>
+  users?.filter((user) => user.id !== userId)
 
 export const updateUserListsAfterBlock = (userId: string) => {
   const { potentialFriends, setPotentialFriends } =
@@ -8,8 +12,11 @@ export const updateUserListsAfterBlock = (userId: string) => {
   const updatedFriends = potentialFriends?.filter((f) => f.id !== userId)
   setPotentialFriends(updatedFriends)
 
-  mutate(PROFILE_ENDPOINTS.nearest)
-  mutate(LIKE_ENDPOINTS.onMe)
+  mutate(PROFILE_ENDPOINTS.nearest, removeUser(userId), {
+    revalidate: true,
+  })
+  mutate(LIKE_ENDPOINTS.onMe, removeUser(userId), { revalidate: true })
+
   useMatchesStore.getState().refreshMatches()
 
   useMatchesStore.setState((state) => ({
